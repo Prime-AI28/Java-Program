58c81e95468450169699265cbafd5e18c2fd5b77
Prime <prime79inovative@gmail.com>
2022-07-12 09:00:38 +0530

    maths java class

---
+++ Self/Maths.java
@@ -0,0 +1,13 @@
+package Self;
+
+public class Maths {
+
+  public static void main(String[] args) {
+    //Maths
+    // Min & Max
+    System.out.println(Math.min(5,8));
+    
+    //Random
+    System.out.println(Math.random()*100);
+  }
+}
