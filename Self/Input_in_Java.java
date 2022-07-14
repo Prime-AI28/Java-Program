16da3e3b00c5cac2a80da8663ae21a62f935df7a
Prime <prime79inovative@gmail.com>
2022-07-14 09:55:05 +0530

    new

---
+++ Self/Input_in_Java.java
@@ -0,0 +1,31 @@
+package Self;
+
+import java.net.SocketPermission;
+import java.util.Scanner;
+
+public class Input_in_Java {
+    
+    public static void main(String[] args){
+        //How to take Input?
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input Your Age :");
+        //int age = sc.nextInt(); // taking int input
+        float age = sc.nextFloat(); // taking float input
+        System.out.println(age);
+
+        // taking stirng(single word) input
+        System.out.print("Input Your Name :");
+        String name = sc.next(); 
+        // next() only takes token i.e. words as an input. Eg: Aman, Ayush are valid inputs. & "ayush is smart" is invalid.
+        System.out.println(name);
+
+        //taking string line input
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter the sentence: ");
+        String line = scn.nextLine();
+        System.out.println(line);
+        sc.close();
+        scn.close();
+
+    }
+}
